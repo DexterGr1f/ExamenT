5eeb72cc8757df7ddd9d33edcf7524fc4e17b55c
Marcos <marquitosceballos15@gmail.com>
2022-12-11 21:45:24 -0300

    QR Implementados
    ESTA CASI LISTO CTM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+/* QR */
+const contenedorQR = document.getElementById('contenedorQR');
+new QRCode(contenedorQR,'https://drive.google.com/file/d/1wI_lbXmZuKzGKjTRg0vUVKiZmCylpJhe/view?usp=share_link');
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
